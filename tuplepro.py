78cd41c554e9c83fd4bf23c57daacb69a93b8f9e
Rekha <saipadmarekha@gmail.com>
2020-05-30 08:10:56 +0530

    Initial commit

---
+++ tuplepro.py
@@ -0,0 +1,10 @@
+tup1 = (12, 34.56);
+tup2 = ('abc', 'xyz');
+print("tup1[0]: ", tup1[0])
+tup=tup1+tup2;
+print(tup)
+print("maximum value in tuple=",max(tup1))
+print("maximum value in tuple=",max(tup2))
+a=tuple(tup)
+print("list=",a)
+print("length of tuple=",len(tup))
